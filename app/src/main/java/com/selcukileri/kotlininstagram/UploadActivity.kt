aa5ae99c8d9b2beccb3ee051d76c3367d3aeaeea
Selçuk İleri <selcukileri98@gmail.com>
2023-10-16 01:14:19 +0300

    signin signup signout completed

---
+++ app/src/main/java/com/selcukileri/kotlininstagram/UploadActivity.kt
@@ -0,0 +1,23 @@
+package com.selcukileri.kotlininstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.selcukileri.kotlininstagram.databinding.ActivityUploadBinding
+
+class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+    fun upload(view: View) {
+
+    }
+    fun selectImage(view: View) {
+
+    }
+}
